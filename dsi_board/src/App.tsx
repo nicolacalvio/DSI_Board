dc055cc9008b95db4f659adb53c9cb5c4eb8873b
nicola_calvio <info@nicolacalvio.com>
2022-02-10 16:59:32 +0100

    initial commit

---
+++ dsi_board/src/App.tsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Header from "./components/Header";
+import './App.css';
+
+
+class App extends Component<any, any> {
+state = {}
+
+  render(){
+      return (
+          <>
+              <Header />
+
+          </>
+      );
+  }
+
+}
+
+export default App;
