4223957242606ca116465231b7dd37f99644ff47
Nicola Calvio <info@nicolacalvio.com>
2022-02-11 10:46:22 +0100

    commit

--- dsi_board/src/components/Column.tsx
+++ dsi_board/src/components/Column.tsx
@@ -2,19 +2,24 @@ import React, {Component} from 'react';
 import Card from './Card';
 
 
-
+interface State {
+    cards: { id:number, description:string }[];
+}
 
 class Column extends Component<any, any> {
-    state = {
+
+    state: Readonly<State> = {
         cards: []
     }
+
     addCard = (idCard:any) => {
-        const cards:any = [...this.state.cards];
+        const cards = [...this.state.cards];
         cards.push(
-            {id:idCard,descrizione:"ciao"}
+            {id:idCard,description:"ciao"}
         )
         this.setState({cards})
     }
+    contatore =0;
 
     render(){
         return (
@@ -22,14 +27,16 @@ class Column extends Component<any, any> {
                 <main>
                     <div className="min-h-md max-w-xs ml-12">
                         <div className="px-4 py-6 sm:px-0">
-                            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
-                                <div className="flex justify-center text-black bg-gray-200 rounded-sm">{this.props.title}</div>
-                           </div>
-                            <div className="flex justify-center">
+                            <div className="border-4 border-dashed  h-32">
+                                <div className="flex justify-center text-black bg-gray-200 rounded-sm border-gray-200 rounded-lg">{this.props.title}</div>
                                 {this.state.cards.map(card=>(
-                                    <Card key={1} cardTitle="Ciao" openDesc="auuu"/>
+                                    <Card key={card.id} cardTitle="Ciao" openDesc={()=>{console.log("ciao")}}/>
                                 ))}
-                                <button className="relative bottom-6 text-white" onClick={()=>this.addCard}>Add</button>
+                           </div>
+
+
+                            <div className="flex justify-center">
+                                <button className="relative bottom-6 text-white" onClick={()=>this.addCard(this.contatore++)}>Add</button>
                             </div>
 
                         </div>
