81fe4e1aaf1c4257862e92a3323f99b5d479ea40
Max <max-bonesso@hotmail.it>
2022-02-11 15:47:29 +0100

    close popup

--- dsi_board/src/components/DescriptionCard.tsx
+++ dsi_board/src/components/DescriptionCard.tsx
@@ -31,16 +31,23 @@ class DescriptionCard extends Component<any, any> {
         }
     }
 
+    close = () => {
+        if(document.getElementById("popup") != null){
+            (document.getElementById("popup") as HTMLFormElement).style.display="none";
+        }
+        
+    }
+
     render(){
         return (
             <>
-                <div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+                <div className="fixed z-10 inset-0 overflow-y-auto" id="popup" aria-labelledby="modal-title" role="dialog" aria-modal="true">
                     <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true">
                         </div>
                         <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
                             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                <img src={x} className='w-6 h-6 absolute top-4 right-4' onClick={()=>this.props.close}/>
+                                <img src={x} className='w-6 h-6 absolute top-4 right-4' onClick={()=>this.close()}/>
                                 <div className="sm:flex sm:items-start">
                                     {this.state.titolo}
                                 </div>
