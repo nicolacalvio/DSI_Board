287b6e3c71c81ddcee4599bc06476dc1d727f73f
Nicola Calvio <info@nicolacalvio.com>
2022-02-11 09:47:13 +0100

    Creazione colonna

--- dsi_board/src/App.tsx
+++ dsi_board/src/App.tsx
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
 import Header from "./components/Header";
+import Column from "./components/Column";
 import './App.css';
 
 
 class App extends Component<any, any> {
 state = {}
 
+addCard:any = (idCard:number) =>{
+
+}
+
   render(){
       return (
           <>
               <Header />
+              <div className="grid grid-cols-4 gap-4">
+                <Column title="TO DO" addCard={this.addCard(1)}/>
+              </div>
           </>
       );
   }
---
+++ dsi_board/src/components/Column.tsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+
+class Column extends Component<any, any> {
+    state = {}
+
+    render(){
+        return (
+            <>
+                <main>
+                    <div className="min-h-md max-w-xs ml-12">
+                        <div className="px-4 py-6 sm:px-0">
+                            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
+                                <div className="flex justify-center text-black bg-gray-200 rounded-sm">{this.props.title}</div>
+
+                           </div>
+                            <div className="relative flex bottom-6 justify-center text-white" onClick={()=>this.props.addCard(this.props.idCard)}>Add</div>
+                        </div>
+                    </div>
+                </main>
+            </>
+        );
+    }
+
+}
+
+export default Column;
