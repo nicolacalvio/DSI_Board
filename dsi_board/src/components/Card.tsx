1ccdef8fc9e81b195ba7c64340e2d9445b3ffaac
Nicola Calvio <info@nicolacalvio.com>
2022-02-11 11:26:47 +0100

    card focus

--- dsi_board/src/components/Card.tsx
+++ dsi_board/src/components/Card.tsx
@@ -3,13 +3,25 @@ import React, {Component} from 'react';
 
 class Card extends Component<any, any> {
     state = {}
-
     render(){
+        let titolo;
+        if (this.props.focus) {
+            titolo = (
+                <input className='mx-2 my-4 h-12 p-4 align-middle flex-auto overflow-hidden text-ellipsis text-base text-neutral-900' type="text">Insert the name of the card</input>
+            );
+            document.querySelectorAll("input").forEach((el)=>{
+                el.focus();
+            })
+        } else {
+            titolo = (
+                <div className='mx-2 my-4 h-12 p-4 align-middle flex-auto overflow-hidden text-ellipsis text-base text-neutral-900'>this.props.cardTitle</div>
+            );
+        }
         return (
             <>
             
                 <div className='w-h-full h-16 mx-4 my-4 bg-zinc-100 rounded-lg' onClick={()=> this.props.openDesc}>
-                    {(this.props.focus) ? (<input className='mx-2 my-4 h-12 p-4 align-middle flex-auto overflow-hidden text-ellipsis text-base text-neutral-900' type="text">Insert the name of the card</input>) : (<div className='mx-2 my-4 h-12 p-4 align-middle flex-auto overflow-hidden text-ellipsis text-base text-neutral-900'>this.props.cardTitle</div>)}
+                    {titolo}
                 </div>
 
             </>
