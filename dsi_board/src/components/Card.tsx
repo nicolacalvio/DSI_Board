3df29162194e134053810108d2d68e47c881ccd9
Max <max-bonesso@hotmail.it>
2022-02-11 11:20:53 +0100

    commit first run

--- dsi_board/src/components/Card.tsx
+++ dsi_board/src/components/Card.tsx
@@ -7,10 +7,11 @@ class Card extends Component<any, any> {
     render(){
         return (
             <>
+            
                 <div className='w-h-full h-16 mx-4 my-4 bg-zinc-100 rounded-lg' onClick={()=> this.props.openDesc}>
-                    <div className='mx-2 my-4 h-12 p-4 align-middle flex-auto overflow-hidden text-ellipsis text-base text-neutral-900'>{this.props.cardTitle}</div>
+                    {(this.props.focus) ? (<input className='mx-2 my-4 h-12 p-4 align-middle flex-auto overflow-hidden text-ellipsis text-base text-neutral-900' type="text">Insert the name of the card</input>) : (<div className='mx-2 my-4 h-12 p-4 align-middle flex-auto overflow-hidden text-ellipsis text-base text-neutral-900'>this.props.cardTitle</div>)}
                 </div>
-                {(this.props.focus) ? (<input type="text">Insert the name of the card</input>) : (<div className='mx-2 my-4 h-12 p-4 align-middle flex-auto overflow-hidden text-ellipsis text-base text-neutral-900'>this.props.cardTitle</div>)}
+
             </>
         );
     }
