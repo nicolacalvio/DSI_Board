8ae48d9011104e4910abddf3e71c4a300ead2e16
Max <max-bonesso@hotmail.it>
2022-02-16 15:55:27 +0100

    Dynamic column part1

--- dsi_board/src/App.tsx
+++ dsi_board/src/App.tsx
@@ -3,19 +3,49 @@ import Header from "./components/Header";
 import Column from "./components/Column";
 import './App.css';
 
+interface StateCol {
+  column: { id:number, columnTitle:string, focus:boolean}[];
+}
 
 class App extends Component<any, any> {
-state = {}
+  
+  state: Readonly<StateCol> = {
+    column: []
+  }
+
+  addColumn = () => {
+    const column = [...this.state.column];
+    const columnTitle:string = "";
+    const length:number = column.length;
+    column.push(
+        {id:length, columnTitle:columnTitle, focus:true}
+    )
+    this.setState({column})
+  }
+
+  modifyTitle = (idTitle:number, title:string) =>{
+    const column = [...this.state.column];
+    column[idTitle].columnTitle = title
+    this.setState({column})
+}
+
+  removeFocus =(key:number) =>{
+    const column = [...this.state.column];
+    column[key].focus = false
+    this.setState({column})
+  }
 
   render(){
       return (
           <>
               <Header />
+              <div className='ml-12 mt-6'>
+                <button className='bg-white h-8 w-32 text-black rounded-md hover:bg-cyan-100' onClick={()=>this.addColumn()}>Add column</button>
+              </div>
               <div className="grid grid-cols-4 gap-0">
-                <Column title="TO DO"/>
-                  <Column title="Doing"/>
-                  <Column title="BackLog"/>
-                  <Column title="DONE"/>
+                {this.state.column.map(col=>
+                                    <Column key={col.id} id={col.id} focus={col.focus} removeFocus={this.removeFocus} title={col.columnTitle} modifyTitle={this.modifyTitle} />)
+                }
               </div>
           </>
       );
